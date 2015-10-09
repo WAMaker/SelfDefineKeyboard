dd27f074494fad6a441f9bb896b23953fccfa578
WAMaker <wamakerdevelop@gmail.com>
2015-10-10 01:34:45 +0800

    commit changes
    1.change UITextView to UITextField;2.update change text methods;

--- SelfDefineKeyboard/ViewController.m
+++ SelfDefineKeyboard/ViewController.m
@@ -12,9 +12,9 @@
 
 #import "WMKeyKeyboardDefine.h"
 
-@interface ViewController () <UITextViewDelegate>
+@interface ViewController () <UITextFieldDelegate>
 
-@property (strong, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @property (strong, nonatomic) WMKeyboardView *keyboardView;
 
@@ -36,9 +36,7 @@
     CGFloat keyboardViewH = 250;
     self.keyboardView = [WMKeyboardView keyboardViewWithKeyboardType:WMKeyboardTypeNumber];
     self.keyboardView.frame = CGRectMake(keyboardViewX, keyboardViewY, keyboardViewW, keyboardViewH);
-    self.textView.inputView = self.keyboardView;
-    [self.textView.layer setBorderColor:[[UIColor blackColor] CGColor]];
-    [self.textView.layer setBorderWidth:1];
+    self.textField.inputView = self.keyboardView;
     
     WS(weakSelf);
     [self.keyboardView setWMKeyboardBlock:^(WMKeyButtonType type, NSString *text) {
@@ -59,7 +57,7 @@
 - (void)changeTextField:(WMKeyButtonType)type Text:(NSString *)text {
     switch (type) {
         case WMKeyButtonTypeDel: {
-            [self changetext:text InTextView:self.textView];
+            [self changetext:text InTextField:self.textField];
         }
             break;
             
@@ -69,7 +67,7 @@
             break;
             
         case WMKeyButtonTypeOther: {
-            [self changetext:text InTextView:self.textView];
+            [self changetext:text InTextField:self.textField];
         }
             break;
     }
@@ -78,30 +76,46 @@
 /**
  *  修改textView中的文字
  */
-- (void)changetext:(NSString *)text InTextView:(UITextView *)textView {
-    UITextPosition *beginning = textView.beginningOfDocument;
-    UITextPosition *start = textView.selectedTextRange.start;
-    UITextPosition *end = textView.selectedTextRange.end;
-    NSInteger startIndex = [textView offsetFromPosition:beginning toPosition:start];
-    NSInteger endIndex = [textView offsetFromPosition:beginning toPosition:end];
+- (void)changetext:(NSString *)text InTextField:(UITextField *)textField {
+    UITextPosition *beginning = textField.beginningOfDocument;
+    UITextPosition *start = textField.selectedTextRange.start;
+    UITextPosition *end = textField.selectedTextRange.end;
+    NSInteger startIndex = [textField offsetFromPosition:beginning toPosition:start];
+    NSInteger endIndex = [textField offsetFromPosition:beginning toPosition:end];
     
     // 将输入框中的文字分成两部分，生成新字符串，判断新字符串是否满足要求
-    NSString *originText = textView.text;
+    NSString *originText = textField.text;
     NSString *part1 = [originText substringToIndex:startIndex];
     NSString *part2 = [originText substringFromIndex:endIndex];
     
+    NSInteger offset;
+    
+    if (![text isEqualToString:@""]) {
+        offset = text.length;
+    } else {
+        if (startIndex == endIndex) { // 只删除一个字符
+            if (startIndex == 0) {
+                return;
+            }
+            offset = -1;
+            part1 = [part1 substringToIndex:(part1.length - 1)];
+        } else {
+            offset = 0;
+        }
+    }
+    
     NSString *newText = [NSString stringWithFormat:@"%@%@%@", part1, text, part2];
-    textView.text = newText;
+    textField.text = newText;
     
     // 重置光标位置
-    UITextPosition *now = [textView positionFromPosition:start offset:text.length];
-    UITextRange *range = [textView textRangeFromPosition:now toPosition:now];
-    textView.selectedTextRange = range;
+    UITextPosition *now = [textField positionFromPosition:start offset:offset];
+    UITextRange *range = [textField textRangeFromPosition:now toPosition:now];
+    textField.selectedTextRange = range;
 }
 
-#pragma mark - UITextViewDelegate
+#pragma mark - UITextFieldDelegate
 
-- (BOOL)textViewShouldBeginEditing:(UITextView *)textView {
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
     [self.keyboardView exchangeNumber];
     return YES;
 }
